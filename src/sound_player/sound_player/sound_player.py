06b5c13c0d20487ca88b7bc967b8b843fc24911c
Amzi <rayfilm@foxmail.com>
2025-05-26 11:02:25 +0200

    battery warning

--- src/sound_player/sound_player/sound_player.py
+++ src/sound_player/sound_player/sound_player.py
@@ -1,12 +1,16 @@
 import rclpy
 from rclpy.node import Node
 from std_msgs.msg import String
+from sensor_msgs.msg import BatteryState
 import subprocess
+import time
 
 class SoundPlayer(Node):
     def __init__(self):
         super().__init__('sound_player')
         self.get_logger().info("Receiver Ready!")
+
+        # Subscription to trigger external sound play
         self.subscription = self.create_subscription(
             String,
             '/play_sound',
@@ -14,14 +18,44 @@ class SoundPlayer(Node):
             10
         )
 
+        # Subscription to battery info
+        self.battery_sub = self.create_subscription(
+            BatteryState,
+            '/io/power/power_watcher',
+            self.battery_callback,
+            10
+        )
+        self.latest_voltage = None
+        self.low_voltage_threshold = 11.7 # for test reasons. In practice can be around 10.5
+        self.last_warning_time = 0
+        self.warning_interval = 5  # seconds
+
+        # Timer to check battery status
+        self.timer = self.create_timer(5.0, self.check_battery_status)
+
     def play_callback(self, msg):
-        sound_path = msg.data
+        self.play_sound(msg.data)
+
+    def battery_callback(self, msg: BatteryState):
+        self.latest_voltage = msg.voltage
+
+    def check_battery_status(self):
+        if self.latest_voltage is None:
+            return
+        if self.latest_voltage < self.low_voltage_threshold:
+            now = time.time()
+            if now - self.last_warning_time > self.warning_interval:
+                self.get_logger().warn(f"Low voltage detected: {self.latest_voltage:.2f}V")
+                self.play_sound('/home/mirte/mirte_voice/low_battery.mp3')
+                self.last_warning_time = now
+
+    def play_sound(self, sound_path):
         self.get_logger().info(f"Playing sound: {sound_path}")
         try:
             subprocess.run(
                 ['ffplay', '-nodisp', '-autoexit', sound_path],
-                # stdout=subprocess.DEVNULL,
-                # stderr=subprocess.DEVNULL
+                stdout=subprocess.DEVNULL,
+                stderr=subprocess.DEVNULL
             )
         except Exception as e:
             self.get_logger().error(f"Failed to play sound: {e}")
@@ -31,4 +65,5 @@ def main(args=None):
     node = SoundPlayer()
     rclpy.spin(node)
     rclpy.shutdown()
+
 
