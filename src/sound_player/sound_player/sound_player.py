858a6c033878fab43f24df83e8c19102ba268d8f
Amzi <rayfilm@foxmail.com>
2025-06-10 08:05:34 +0200

    new warning sound

--- src/sound_player/sound_player/sound_player.py
+++ src/sound_player/sound_player/sound_player.py
@@ -26,7 +26,8 @@ class SoundPlayer(Node):
             10
         )
         self.latest_voltage = None
-        self.low_voltage_threshold = 11.7 # for test reasons. In practice can be around 10.5
+        # fully charged 12.6v; fully discharged 9v
+        self.low_voltage_threshold = 0.2 # percentage
         self.last_warning_time = 0
         self.warning_interval = 5  # seconds
 
@@ -42,7 +43,8 @@ class SoundPlayer(Node):
     def check_battery_status(self):
         if self.latest_voltage is None:
             return
-        if self.latest_voltage < self.low_voltage_threshold:
+        percentage = (self.latest_voltage-9.0)/3.6
+        if percentage < self.low_voltage_threshold:
             now = time.time()
             if now - self.last_warning_time > self.warning_interval:
                 self.get_logger().warn(f"Low voltage detected: {self.latest_voltage:.2f}V")
